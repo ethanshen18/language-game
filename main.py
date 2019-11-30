d64dedf75cb9cb8b10ec46b75c74502be615d6e6
Ethan <ethan.shen1031@gmail.com>
2019-11-30 14:08:37 -0800

    template finished

--- main.py
+++ main.py
@@ -1,57 +1,13 @@
 # Import Flask
 import flask
-import requests
 
 # Create the application
 app = flask.Flask(__name__)
 
-API_URL = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?inputtype=textquery&locationbias=ipbias&fields=formatted_address,name,rating&"
-
 @app.route('/')
-def hello():
-    return "nwHacks Build Day Vlog!"
+def page():
+	return flask.render_template('language.html')
 
-# path parameters
-@app.route('/<name>')
-def personal_hello(name):
-	return "Hello " + name
-
-# serving hello.html
-@app.route('/fancy/<name>')
-def some_page(name):
-	return flask.render_template('hello.html', name=name)
-
-# serving find.html
-@app.route('/find', methods=['GET'])
-def find():
-	return flask.render_template('find.html')
-
-# process query
-@app.route('/process_query', methods=['POST'])
-def process_query():
-	data = flask.request.form
-	location = data['some_location']
-	print(location)
-
-	requestString = formRequest(location)
-	responses = makeGET(requestString)['candidates']
-	return flask.render_template('find.html', responses=responses)
-
-def formRequest(input):
-	#TODO
-	return API_URL + "key=" + readKey() + "&input=" + input # return some string 
-
-def makeGET(input):
-	response = requests.get(input).json()
-	if response:
-		return response
-	else:
-		return "uh oh" # return some response
-
-def readKey():
-	f = open("secrets.txt", "r")
-	contents = f.read()
-	return contents.strip()
 
 if __name__ == '__main__':
     app.run(debug=True)
