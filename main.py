0a468ef13819a1d668d4a607556ecdd5a62c8df8
Ethan <ethan.shen1031@gmail.com>
2019-11-30 16:35:50 -0800

    Word changes on reload

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Import Flask
 import json
 import flask
+import random
 from yandex_translate import YandexTranslate
 
 # Create the application
@@ -8,16 +9,24 @@ app = flask.Flask(__name__)
 
 @app.route('/')
 def page():
-	lang = 'en-fr'
-	key = 'dog'
-	op1 = 'dog'
-	op2 = 'dog'
-	op3 = 'dog'
-	op4 = 'dog'
-	op5 = 'dog'
+	lang = 'en-zh'
+
+	file = open('ENwords.txt', 'r')
+	words = file.readlines()
+	file.close()
+
+	rand = random.randint(0, len(words))
+
+	key = words[rand]
+	op1 = words[rand]
+	op2 = words[rand]
+	op3 = words[rand]
+	op4 = words[rand]
+	op5 = words[rand]
 	translate = YandexTranslate(readKey())
 	translation = translate.translate(key, lang)
-	return flask.render_template('index.html', word = translation['text'][0].upper(), op1 = op1, op2 = op2, op3 = op3, op4 = op4, op5 = op5)
+	return flask.render_template('index.html', word = translation['text'][0], 
+		op1 = op1, op2 = op2, op3 = op3, op4 = op4, op5 = op5)
 
 def readKey():
 	f = open("secrets.txt", "r")
